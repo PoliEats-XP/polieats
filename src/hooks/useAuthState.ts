eaa4bc73a6d9488a6194c287c4537195b2e4bb1f
Guilherme Brasil <dev.guilhermebrasil@gmail.com>
2025-04-24 21:31:34 -0300

    lint: fix linting

--- src/hooks/useAuthState.ts
+++ src/hooks/useAuthState.ts
@@ -1,15 +1,23 @@
-import { useState } from "react";
+import { useState } from 'react'
 
 export const useAuthState = () => {
-    const [error, setError] = useState('');
-    const [success, setSuccess] = useState('');
-    const [loading,  setLoading] = useState(false); 
+	const [error, setError] = useState('')
+	const [success, setSuccess] = useState('')
+	const [loading, setLoading] = useState(false)
 
-    const resetState = () => {
-        setError('');
-        setSuccess('');
-        setLoading(false);
-    };
+	const resetState = () => {
+		setError('')
+		setSuccess('')
+		setLoading(false)
+	}
 
-    return {error, setError, success, setSuccess, loading, setLoading, resetState};
-};
+	return {
+		error,
+		setError,
+		success,
+		setSuccess,
+		loading,
+		setLoading,
+		resetState,
+	}
+}
--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -3,12 +3,7 @@ import { createAuthClient } from 'better-auth/react'
 // auth instance used on client side
 
 export const authClient = createAuthClient({
-  baseURL: process.env.BETTER_AUTH_URL!
+	baseURL: process.env.BETTER_AUTH_URL!,
 })
 
-export const {
-  signIn,
-  signOut,
-  signUp,
-  useSession
-} = authClient; 
+export const { signIn, signOut, signUp, useSession } = authClient
