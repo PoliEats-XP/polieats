c295fbcda8ce49825d0be2778fb351842c22e108
Guilherme Brasil <dev.guilhermebrasil@gmail.com>
2025-04-30 09:29:13 -0300

    feat: add admin plugin to manage user roles

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,9 +1,11 @@
 import { createAuthClient } from 'better-auth/react'
+import { adminClient } from 'better-auth/client/plugins'
 
 // auth instance used on client side
 
 export const authClient = createAuthClient({
 	baseURL: process.env.BETTER_AUTH_URL!,
+	plugins: [adminClient()],
 })
 
 export const { signIn, signOut, signUp, useSession } = authClient
