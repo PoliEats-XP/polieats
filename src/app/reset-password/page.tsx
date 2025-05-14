7bf34907da4de64608dab9a2a110c8ce8ca7ab21
Guilherme Brasil <dev.guilhermebrasil@gmail.com>
2025-05-14 08:36:19 -0300

    wip: init reset-password page

---
+++ src/app/reset-password/page.tsx
@@ -0,0 +1,20 @@
+import { AuthHero } from '@/components/auth/auth-hero'
+import { toast } from 'sonner'
+
+export default function ResetPassword() {
+	const token = new URLSearchParams(window.location.search).get('token')
+
+	if (!token) {
+		toast.error('Token não encontrado na URL')
+	}
+
+	return (
+		<div className="flex flex-col items-center justify-center h-screen p-4 md:p-0 max-w-md mx-auto">
+			<AuthHero />
+
+			<p className="text-lg text-midgray font-light mb-6 text-center">
+				Redefina sua senha para acessar sua conta
+			</p>
+		</div>
+	)
+}
