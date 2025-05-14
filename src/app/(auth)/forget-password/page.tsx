f4d5f5971312c51312918888f573586d3fe5f82a
Guilherme Brasil <dev.guilhermebrasil@gmail.com>
2025-05-14 08:27:04 -0300

    wip: init forget-password page

---
+++ src/app/(auth)/forget-password/page.tsx
@@ -0,0 +1,13 @@
+import { AuthHero } from '@/components/auth/auth-hero'
+
+export default function ForgetPassword() {
+	return (
+		<div className="flex flex-col items-center justify-center h-screen p-4 md:p-0 max-w-md mx-auto">
+			<AuthHero />
+
+			<p className="text-lg text-midgray font-light mb-6 text-center">
+				Esqueceu sua senha? Não se preocupe!
+			</p>
+		</div>
+	)
+}
